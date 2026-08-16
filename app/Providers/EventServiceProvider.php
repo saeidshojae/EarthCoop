855062a81c2965a329aae71e6099994831fc3f57
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-16 15:10:42 +0330

    Register system artifact realtime feed bridge

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -77,9 +77,11 @@ class EventServiceProvider extends ServiceProvider
         ],
         \App\Events\BlogCreated::class => [
             \App\Listeners\SendBlogCreatedNotifications::class,
+            \App\Listeners\BridgeSystemGroupArtifactToRealtimeFeed::class,
         ],
         \App\Events\PollCreated::class => [
             \App\Listeners\SendPollCreatedNotifications::class,
+            \App\Listeners\BridgeSystemGroupArtifactToRealtimeFeed::class,
         ],
         \App\Events\ElectionStarted::class => [
             \App\Listeners\SendElectionStartedNotifications::class,
