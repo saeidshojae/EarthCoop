66428a8734e3beb71cd79b4a3cbaf704e4b26230
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-18 15:47:20 +0330

    test(najm-hoda): cover complete management console content tools

--- tests/Feature/NajmHoda/ManagementConsoleSourceContractTest.php
+++ tests/Feature/NajmHoda/ManagementConsoleSourceContractTest.php
@@ -10,8 +10,10 @@ class ManagementConsoleSourceContractTest extends TestCase
     {
         $app = file_get_contents(resource_path('js/app.js'));
         $console = file_get_contents(resource_path('js/najm-hoda-management-console-v2.js'));
+        $contentTools = file_get_contents(resource_path('js/najm-hoda-management-content-tools.js'));
 
         $this->assertStringContainsString('import "./najm-hoda-management-console-v2.js";', $app);
+        $this->assertStringContainsString('import "./najm-hoda-management-content-tools.js";', $app);
         $this->assertStringNotContainsString('import "./najm-hoda-management-console.js";', $app);
         $this->assertStringContainsString('GroupChatConfig?.canManageSession', $console);
         $this->assertStringContainsString('[2, 3].includes', $console);
@@ -31,5 +33,19 @@ class ManagementConsoleSourceContractTest extends TestCase
         $this->assertStringContainsString('action_due', $console);
         $this->assertStringContainsString('action_priority', $console);
         $this->assertStringContainsString('@media(max-width:420px)', $console);
+
+        $this->assertStringContainsString('محتوا، تعامل و حکمرانی', $contentTools);
+        $this->assertStringContainsString('ساخت پست', $contentTools);
+        $this->assertStringContainsString('ساخت نظرسنجی', $contentTools);
+        $this->assertStringContainsString('ثبت نظر', $contentTools);
+        $this->assertStringContainsString('ثبت واکنش', $contentTools);
+        $this->assertStringContainsString('ارسال پیام گروه', $contentTools);
+        $this->assertStringContainsString('انتخابات گروه', $contentTools);
+        $this->assertStringContainsString('یک پست بساز | عنوان:', $contentTools);
+        $this->assertStringContainsString('یک نظرسنجی بساز | سوال:', $contentTools);
+        $this->assertStringContainsString("fetch('/api/najm-hoda/chat'", $contentTools);
+        $this->assertStringContainsString("send(panel,widget,'تأیید'", $contentTools);
+        $this->assertStringContainsString('electionVotingOverlay', $contentTools);
+        $this->assertStringContainsString('message_editor', $contentTools);
     }
 }
