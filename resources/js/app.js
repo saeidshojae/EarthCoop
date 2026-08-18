05990ee12358317a731c4d95f6165d12294afbc5
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-18 15:46:48 +0330

    feat(najm-hoda): load guided group content management tools

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,7 @@ import "./najm-bahar.js";
 import "./najm-bahar-membership-source.js";
 import "./najm-hoda-context.js";
 import "./najm-hoda-management-console-v2.js";
+import "./najm-hoda-management-content-tools.js";
 import "./najm-hoda-attention-panel.js";
 import { register } from "swiper/element/bundle";
 import "./group-chat/index.js";
