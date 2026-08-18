3dcfe442f7b78ad0effac82b4cef110e89415b4f
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-18 12:59:52 +0330

    Allow canonical -test database in test safety guard

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,9 +14,13 @@ abstract class TestCase extends BaseTestCase
 
         $database = (string) config('database.connections.'.config('database.default').'.database');
 
-        if ($database !== ':memory:' && ! str_ends_with($database, '_testing')) {
+        $isSafeTestDatabase = $database === ':memory:'
+            || str_ends_with($database, '_testing')
+            || str_ends_with($database, '-test');
+
+        if (! $isSafeTestDatabase) {
             throw new \RuntimeException(
-                "Unsafe test database [{$database}]. Use an in-memory database or a database ending in _testing."
+                "Unsafe test database [{$database}]. Use an in-memory database or a database ending in _testing or -test."
             );
         }
     }
