ff98b40b21a6a7dd0418973b1cbdcd806a94c1d6
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-20 04:26:00 +0330

    feat(stock): load canonical Stock routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,6 +37,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
 
             Route::middleware('web')
+                ->group(base_path('routes/stock-canonical.php'));
+
+            Route::middleware('web')
                 ->group(base_path('routes/najm-hoda-group-attention.php'));
 
             Route::middleware('web')
