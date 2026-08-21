2fc688671340e9e38fd6a08c88cd501d8362cbdc
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-21 21:45:57 +0330

    E8 model inherited electoral appointments

--- app/Models/ElectionAppointment.php
+++ app/Models/ElectionAppointment.php
@@ -8,8 +8,9 @@ class ElectionAppointment extends Model
 {
     protected $fillable = [
         'election_id', 'responsibility_offer_id', 'user_id', 'group_id',
-        'position', 'group_role', 'status', 'appointed_at', 'ended_at',
-        'superseded_by_appointment_id', 'actor', 'reason', 'metadata',
+        'position', 'group_role', 'appointment_kind', 'source_appointment_id',
+        'status', 'appointed_at', 'ended_at', 'superseded_by_appointment_id',
+        'actor', 'reason', 'metadata',
     ];
 
     protected $casts = [
@@ -23,4 +24,6 @@ class ElectionAppointment extends Model
     public function user() { return $this->belongsTo(User::class); }
     public function group() { return $this->belongsTo(Group::class); }
     public function representation() { return $this->hasOne(ElectionRepresentationAssignment::class, 'appointment_id'); }
+    public function sourceAppointment() { return $this->belongsTo(self::class, 'source_appointment_id'); }
+    public function inheritedAppointments() { return $this->hasMany(self::class, 'source_appointment_id'); }
 }
