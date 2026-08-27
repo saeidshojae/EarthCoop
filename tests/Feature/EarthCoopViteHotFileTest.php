7127ad5a7e0045ede2c3bd612d3c2b572c4437b9
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-27 18:37:37 +0330

    test(assets): cover stale local Vite hot fallback

---
+++ tests/Feature/EarthCoopViteHotFileTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Support\EarthCoopVite;
+use Illuminate\Foundation\Vite;
+use Tests\TestCase;
+
+class EarthCoopViteHotFileTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        @unlink(public_path('hot'));
+        parent::tearDown();
+    }
+
+    public function test_stale_local_hot_file_falls_back_to_manifest_build(): void
+    {
+        file_put_contents(public_path('hot'), 'http://127.0.0.1:65534');
+
+        $vite = app(Vite::class);
+
+        $this->assertInstanceOf(EarthCoopVite::class, $vite);
+        $this->assertFalse($vite->isRunningHot());
+        $this->assertFileDoesNotExist(public_path('hot'));
+    }
+}
