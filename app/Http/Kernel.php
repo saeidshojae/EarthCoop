2825df9261f4f0c272640d82975f0cec1c3f879d
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-29 22:45:32 +0330

    refactor(stock): remove redundant canonical valuation middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\CanonicalStockAdminValuation::class,
             \App\Http\Middleware\UpdateLastSeen::class,
             \App\Http\Middleware\SetLocale::class,
         ],
