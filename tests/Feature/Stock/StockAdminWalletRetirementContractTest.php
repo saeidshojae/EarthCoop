c4c6c1caf11cbd4714c2be2bb4be7dd0038790bb
saeid shojaei <s.s.sh.kia@gmail.com>
2026-08-30 02:20:17 +0330

    test(stock): require canonical admin dashboard controller binding

--- tests/Feature/Stock/StockAdminWalletRetirementContractTest.php
+++ tests/Feature/Stock/StockAdminWalletRetirementContractTest.php
@@ -8,9 +8,14 @@ class StockAdminWalletRetirementContractTest extends TestCase
 {
     public function test_admin_stock_dashboard_resolves_canonical_module_view_and_has_no_wallet_or_rial_surface(): void
     {
-        $controller = file_get_contents(app_path('Modules/Stock/Controllers/StockController.php'));
+        $provider = file_get_contents(app_path('Providers/StockExternalCapitalServiceProvider.php'));
+        $controllerPath = app_path('Modules/Stock/Controllers/CanonicalStockAdminController.php');
         $view = file_get_contents(app_path('Modules/Stock/Views/admin_stock_info.blade.php'));
 
+        $this->assertFileExists($controllerPath);
+        $controller = file_get_contents($controllerPath);
+
+        $this->assertStringContainsString('CanonicalStockAdminController::class', $provider);
         $this->assertStringContainsString("view('Stock::admin_stock_info'", $controller);
         $this->assertStringNotContainsString("view('stock.admin_stock_info'", $controller);
 
