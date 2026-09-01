dcbec2dbb302453b329867d64e29f790639cd85e
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-01 12:39:04 +0330

    feat(reputation): link point consumption to conversion identity

--- app/Models/UserPointConsumption.php
+++ app/Models/UserPointConsumption.php
@@ -8,10 +8,16 @@ class UserPointConsumption extends Model
 {
     protected $fillable = [
         'user_id',
+        'user_point_conversion_id',
         'user_point_transaction_id',
         'points_consumed',
         'conversion_key',
         'policy_version_id',
         'policy_version',
     ];
+
+    public function conversion()
+    {
+        return $this->belongsTo(UserPointConversion::class, 'user_point_conversion_id');
+    }
 }
