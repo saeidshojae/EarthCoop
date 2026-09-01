46275ce900c8932a825d48d423539b2eecd71994
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-01 18:39:38 +0330

    test(reputation): fix conversion info contract literals

--- tests/Feature/Reputation/ParticipationConversionInfoTransparencyContractTest.php
+++ tests/Feature/Reputation/ParticipationConversionInfoTransparencyContractTest.php
@@ -11,14 +11,14 @@ class ParticipationConversionInfoTransparencyContractTest extends TestCase
         $controller = file_get_contents(app_path('Http/Controllers/ReputationConversionController.php'));
 
         foreach ([
-            "'total_points' => $totalPoints",
-            "'uncashed_points' => $uncashedPoints",
-            "'cashed_points' => $cashedPoints",
-            "'convertible_awarded_points' => $pointSummary['convertible_awarded_points']",
-            "'ledger_consumed_points' => $pointSummary['ledger_consumed_points']",
-            "'legacy_cashed_points' => $pointSummary['legacy_cashed_points']",
-            "'participation_reversal_points' => $pointSummary['participation_reversal_points']",
-            "'remaining_convertible_points' => $pointSummary['remaining_convertible_points']",
+            "'total_points' => \$totalPoints",
+            "'uncashed_points' => \$uncashedPoints",
+            "'cashed_points' => \$cashedPoints",
+            "'convertible_awarded_points' => \$pointSummary['convertible_awarded_points']",
+            "'ledger_consumed_points' => \$pointSummary['ledger_consumed_points']",
+            "'legacy_cashed_points' => \$pointSummary['legacy_cashed_points']",
+            "'participation_reversal_points' => \$pointSummary['participation_reversal_points']",
+            "'remaining_convertible_points' => \$pointSummary['remaining_convertible_points']",
         ] as $contract) {
             $this->assertStringContainsString($contract, $controller);
         }
