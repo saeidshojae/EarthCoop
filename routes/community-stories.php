f6824ac9f88982abc042b2bda401e16391610ce4
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-07 19:03:35 +0330

    feat: add community story moderation routes

--- routes/community-stories.php
+++ routes/community-stories.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\CommunityStoryController as AdminCommunityStoryController;
 use App\Http\Controllers\Profile\CommunityStoryController;
+use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +11,14 @@ Route::middleware(Authenticate::class)->prefix('community-stories')->name('commu
     Route::post('/', [CommunityStoryController::class, 'store'])->name('store');
     Route::post('/{communityStory}/withdraw', [CommunityStoryController::class, 'withdraw'])->name('withdraw');
 });
+
+Route::middleware([Authenticate::class, AdminMiddleware::class])
+    ->prefix('admin/community-stories')
+    ->name('admin.community-stories.')
+    ->group(function () {
+        Route::get('/', [AdminCommunityStoryController::class, 'index'])->name('index');
+        Route::post('/{communityStory}/approve', [AdminCommunityStoryController::class, 'approve'])->name('approve');
+        Route::post('/{communityStory}/reject', [AdminCommunityStoryController::class, 'reject'])->name('reject');
+        Route::post('/{communityStory}/feature', [AdminCommunityStoryController::class, 'feature'])->name('feature');
+        Route::post('/{communityStory}/unfeature', [AdminCommunityStoryController::class, 'unfeature'])->name('unfeature');
+    });
