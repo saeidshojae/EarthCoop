2b1af80540278cb8eeb867a52efd6daff49484e6
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-10 02:05:26 +0330

    feat: relate locations to governance areas

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Location extends Model
@@ -75,5 +76,10 @@ class Location extends Model
     public function incomingRelations(): HasMany
     {
         return $this->hasMany(LocationRelation::class, 'to_location_id');
+    }
+
+    public function governanceAreas(): BelongsToMany
+    {
+        return $this->belongsToMany(GovernanceArea::class, 'governance_area_locations')->withTimestamps();
     }
 }
