c2daae14945e2981fd5012d94ea85fe266864d86
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-11 01:06:53 +0330

    feat: add project governance cutover flag

--- config/location-governance.php
+++ config/location-governance.php
@@ -7,4 +7,5 @@ return [
     'registration_enabled' => filter_var(env('LOCATION_GOVERNANCE_REGISTRATION_ENABLED', false), FILTER_VALIDATE_BOOLEAN),
     'groups_enabled' => filter_var(env('LOCATION_GOVERNANCE_GROUPS_ENABLED', false), FILTER_VALIDATE_BOOLEAN),
     'elections_enabled' => filter_var(env('LOCATION_GOVERNANCE_ELECTIONS_ENABLED', false), FILTER_VALIDATE_BOOLEAN),
+    'projects_enabled' => filter_var(env('LOCATION_GOVERNANCE_PROJECTS_ENABLED', false), FILTER_VALIDATE_BOOLEAN),
 ];
