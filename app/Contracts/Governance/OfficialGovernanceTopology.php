034e3bee801caaad8eeed73fd396452c885dfe2f
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-10 20:42:47 +0330

    feat: bind official governance topology

--- app/Contracts/Governance/OfficialGovernanceTopology.php
+++ app/Contracts/Governance/OfficialGovernanceTopology.php
@@ -3,8 +3,11 @@
 namespace App\Contracts\Governance;
 
 use App\Models\GovernanceArea;
+use App\Services\Elections\GovernanceElectionTopology;
+use Illuminate\Container\Attributes\Bind;
 use Illuminate\Support\Collection;
 
+#[Bind(GovernanceElectionTopology::class)]
 interface OfficialGovernanceTopology
 {
     public function parentOf(GovernanceArea $area): ?GovernanceArea;
