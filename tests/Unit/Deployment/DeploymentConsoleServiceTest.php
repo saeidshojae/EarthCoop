36697d03268784192c2da36097e62dcc9d8e8e5c
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-12 01:33:28 +0330

    test(deploy): require guarded governance topology operations

--- tests/Unit/Deployment/DeploymentConsoleServiceTest.php
+++ tests/Unit/Deployment/DeploymentConsoleServiceTest.php
@@ -15,18 +15,22 @@ class DeploymentConsoleServiceTest extends TestCase
         $this->assertSame([
             'migration_status',
             'reference_dry_run',
+            'topology_dry_run',
             'readiness',
             'flag_status',
             'migrate',
             'bootstrap',
             'reference_apply',
+            'topology_apply',
         ], array_keys($service->operations()));
 
         $this->assertSame('MIGRATE', $service->confirmationFor('migrate'));
         $this->assertSame('BOOTSTRAP', $service->confirmationFor('bootstrap'));
         $this->assertSame('APPLY-IR', $service->confirmationFor('reference_apply'));
+        $this->assertSame('APPLY-GOV-IR', $service->confirmationFor('topology_apply'));
         $this->assertNull($service->confirmationFor('migration_status'));
         $this->assertNull($service->confirmationFor('reference_dry_run'));
+        $this->assertNull($service->confirmationFor('topology_dry_run'));
         $this->assertNull($service->confirmationFor('readiness'));
         $this->assertNull($service->confirmationFor('flag_status'));
     }
