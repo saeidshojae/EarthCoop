8817e44f467d2ac694da4da329698e20be3f0824
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-11 04:20:53 +0330

    feat(location): route authenticated geolocation assist

--- routes/location-governance.php
+++ routes/location-governance.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LocationGovernance\GeolocationController;
 use App\Http\Controllers\LocationGovernance\LocationOptionsController;
 use App\Http\Controllers\LocationGovernance\ProfileEditController;
 use App\Http\Controllers\LocationGovernance\ProfileResidenceController;
@@ -9,6 +10,9 @@ Route::prefix('location/options')->name('location.options.')->group(function () 
     Route::get('/root', [LocationOptionsController::class, 'root'])->name('root');
     Route::get('/{location}/children', [LocationOptionsController::class, 'children'])->name('children');
 });
+
+Route::middleware('auth')->post('/location-governance/geolocation/match', [GeolocationController::class, 'match'])
+    ->name('location.geolocation.match');
 
 // Loaded after routes/web.php. These deliberately shadow the legacy profile
 // location endpoints. Each canonical adapter delegates straight back to the
