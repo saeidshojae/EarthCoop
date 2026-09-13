e42e4f0b65d57293d7c7290d8e2341edc79b0095
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-13 22:05:02 +0330

    test: require proposal-aware canonical residence views

--- tests/Feature/LocationGovernance/CanonicalResidenceUiContractTest.php
+++ tests/Feature/LocationGovernance/CanonicalResidenceUiContractTest.php
@@ -29,6 +29,8 @@ class CanonicalResidenceUiContractTest extends TestCase
         foreach ([$registration, $profile] as $view) {
             $this->assertStringContainsString('data-location-selector', $view);
             $this->assertStringContainsString('name="location_id"', $view);
+            $this->assertStringContainsString('name="location_proposal_id"', $view);
+            $this->assertStringContainsString('data-location-proposal-id', $view);
         }
 
         $this->assertStringContainsString('data-location-selector-context="registration"', $registration);
@@ -36,7 +38,7 @@ class CanonicalResidenceUiContractTest extends TestCase
     }
 
     #[Test]
-    public function shared_selector_consumes_only_the_schema_driven_location_api_and_endpoint_metadata(): void
+    public function shared_selector_consumes_schema_driven_picker_contract_and_proposal_endpoint(): void
     {
         $selectorPath = resource_path('js/location-selector.js');
         $this->assertFileExists($selectorPath);
@@ -46,8 +48,11 @@ class CanonicalResidenceUiContractTest extends TestCase
 
         $this->assertStringContainsString('/location/options/root', $selector);
         $this->assertStringContainsString('/children', $selector);
+        $this->assertStringContainsString('/locations/proposals', $selector);
         $this->assertStringContainsString('is_residence_endpoint', $selector);
+        $this->assertStringContainsString('allowed_types', $selector);
         $this->assertStringContainsString('location_id', $selector);
+        $this->assertStringContainsString('location_proposal_id', $selector);
         $this->assertStringNotContainsString('/api/locations?level=', $selector);
         $this->assertStringContainsString('location-selector.js', $app);
     }
