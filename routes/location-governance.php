ba17e00d2bf9e026df03a6b95be36eded2e08d04
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-15 02:05:46 +0330

    feat: add project scope traversal endpoints

--- routes/location-governance.php
+++ routes/location-governance.php
@@ -7,11 +7,18 @@ use App\Http\Controllers\LocationGovernance\LocationOptionsController;
 use App\Http\Controllers\LocationGovernance\MyLocationGovernanceController;
 use App\Http\Controllers\LocationGovernance\ProfileEditController;
 use App\Http\Controllers\LocationGovernance\ProfileResidenceController;
+use App\Http\Controllers\LocationGovernance\ProjectScopeOptionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('location/options')->name('location.options.')->group(function () {
     Route::get('/root', [LocationOptionsController::class, 'root'])->name('root');
     Route::get('/{location}/children', [LocationOptionsController::class, 'children'])->name('children');
+});
+
+Route::prefix('location/project-scope/options')->name('location.project-scope.options.')->group(function () {
+    Route::get('/root', [ProjectScopeOptionsController::class, 'root'])->name('root');
+    Route::get('/governance/{governanceArea}/children', [ProjectScopeOptionsController::class, 'children'])
+        ->name('governance.children');
 });
 
 Route::middleware('auth')->post('/location-governance/geolocation/match', [GeolocationController::class, 'match'])
