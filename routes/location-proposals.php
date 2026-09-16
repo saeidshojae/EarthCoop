dc5d1904f8f8b1b5271271112c4d20cb4efc55c9
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-16 19:13:49 +0330

    feat: route pending proposal child options

--- routes/location-proposals.php
+++ routes/location-proposals.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\Location\LocationProposalController;
+use App\Http\Controllers\LocationGovernance\LocationOptionsController;
 use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(Authenticate::class)->group(function () {
     Route::post('/locations/proposals', [LocationProposalController::class, 'store'])
         ->name('locations.proposals.store');
+
+    Route::get('/location/proposals/{locationProposal}/children', [LocationOptionsController::class, 'proposalChildren'])
+        ->name('locations.proposals.children');
 
     Route::post('/locations/proposals/{locationProposal}/support', [LocationProposalController::class, 'support'])
         ->name('locations.proposals.support');
