1c821264cc13d92d7078b24130aa9cd665ea4813
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-19 23:05:51 +0330

    fix: align selector urban region label

--- resources/js/location-selector.js
+++ resources/js/location-selector.js
@@ -11,7 +11,7 @@ import {
 
 const FA_TYPE_LABELS = Object.freeze({
     global: 'جهانی', continent: 'قاره', country: 'کشور', province: 'استان / ایالت', county: 'شهرستان / ناحیه',
-    section: 'بخش', city: 'شهر', rural_district: 'دهستان', village: 'روستا', urban_region: 'منطقه شهری',
+    section: 'بخش', city: 'شهر', rural_district: 'دهستان', village: 'روستا', urban_region: 'منطقه',
     neighborhood: 'محله', street: 'خیابان', alley: 'کوچه', complex: 'مجتمع', building: 'ساختمان',
 });
 const localizeLocationTypeLabel = (type, locale = (typeof document !== 'undefined' ? document.documentElement.lang : 'fa')) => String(locale || '').toLowerCase().startsWith('fa') && FA_TYPE_LABELS[type?.key] ? FA_TYPE_LABELS[type.key] : (type?.label || type?.canonical_name || type?.key || '');
