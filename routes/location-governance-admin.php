7141b8c85faa80a424fc79243345ba7129bf606b
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-19 06:33:43 +0330

    feat: route canonical proposal corrections

--- routes/location-governance-admin.php
+++ routes/location-governance-admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\LocationGovernanceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LocationGovernanceController::class, 'index'])->name('index');
+Route::put('/proposals/{locationProposal}', [LocationGovernanceController::class, 'update'])->name('proposals.update');
 Route::post('/proposals/{locationProposal}/approve', [LocationGovernanceController::class, 'approve'])->name('proposals.approve');
 Route::post('/proposals/{locationProposal}/reject', [LocationGovernanceController::class, 'reject'])->name('proposals.reject');
 Route::post('/proposals/{locationProposal}/merge', [LocationGovernanceController::class, 'merge'])->name('proposals.merge');
