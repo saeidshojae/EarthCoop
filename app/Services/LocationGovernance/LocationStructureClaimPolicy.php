78d6edeb30e38d4c62e8bd2eb63720ab7ab591f3
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-20 04:22:15 +0330

    fix: restore structural claim path contract

--- app/Services/LocationGovernance/LocationStructureClaimPolicy.php
+++ app/Services/LocationGovernance/LocationStructureClaimPolicy.php
@@ -74,14 +74,7 @@ class LocationStructureClaimPolicy
                 return $this->descendantTypeCodes($location, ['urban_region', 'neighborhood']);
             }
 
-            if (in_array('approved', $statuses, true)) {
-                return ['neighborhood'];
-            }
-
-            return array_values(array_unique(array_merge(
-                ['neighborhood'],
-                $this->descendantTypeCodes($location, ['urban_region', 'neighborhood'])
-            )));
+            return ['neighborhood'];
         }
 
         if (in_array($typeKey, ['urban_region', 'village'], true)
